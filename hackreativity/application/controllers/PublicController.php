f8af899d7ae62d595221fcbd96b18352ba0ba4ce
DaviCode <david_95thebest@hotmail.it>
2016-10-29 13:04:24 +0200

    Hackreativity CebaRox

---
+++ hackreativity/application/controllers/PublicController.php
@@ -0,0 +1,72 @@
+<?php
+
+class PublicController extends Zend_Controller_Action
+{
+	protected $_catalogModel;
+	protected $_authService;
+	protected $_form;
+	
+    public function init()
+    {
+		$this->_helper->layout->setLayout('main');
+        $this->_catalogModel = new Application_Model_Catalog();
+        $this->_authService = new Application_Service_Auth();
+        $this->view->loginForm = $this->getLoginForm();
+    }
+
+    public function indexAction()
+    {
+    }
+ 	
+    public function viewstaticAction () {
+    	$page = $this->_getParam('staticPage');
+    	$this->render($page);
+    }
+    
+    public function loginAction()
+    {}
+
+    public function authenticateAction()
+	{        
+        $request = $this->getRequest();
+        if (!$request->isPost()) {
+            return $this->_helper->redirector('login');
+        }
+        $form = $this->_form;
+        if (!$form->isValid($request->getPost())) {
+            $form->setDescription('Attenzione: alcuni dati inseriti sono errati.');
+        	return $this->render('login');
+        }
+        if (false === $this->_authService->authenticate($form->getValues())) {
+            $form->setDescription('Autenticazione fallita. Riprova');
+            return $this->render('login');
+        }
+        return $this->_helper->redirector('index', $this->_authService->getIdentity()->role);
+	}
+
+	// Validazione AJAX
+	public function validateloginAction() 
+    {
+        $this->_helper->getHelper('layout')->disableLayout();
+    		$this->_helper->viewRenderer->setNoRender();
+
+        $loginform = new Application_Form_Public_Auth_Login();
+        $response = $loginform->processAjax($_POST); 
+        if ($response !== null) {
+        	$this->getResponse()->setHeader('Content-type','application/json')->setBody($response);        	
+        }
+    }
+	
+	private function getLoginForm()
+    {
+    	$urlHelper = $this->_helper->getHelper('url');
+		$this->_form = new Application_Form_Public_Auth_Login();
+    	$this->_form->setAction($urlHelper->url(array(
+			'controller' => 'public',
+			'action' => 'authenticate'),
+			'default'
+		));
+		return $this->_form;
+    }   	
+}
+
---
+++ hackreativity/include/connectZP.php
@@ -0,0 +1,6 @@
+<?php 
+$USER="root";
+$DB="hackreativity";
+$HOST="localhost";
+$PASSWORD="root";
+?>
